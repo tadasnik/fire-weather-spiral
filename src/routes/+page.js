52fb17d86264ebfa947fd7e7bce94f9d3ba29fc2
tadasnik <tadas.nik@gmail.com>
2023-11-05 06:49:40 +0000

    fetch data firebase

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,14 +1,14 @@
-// /** @type {import('./$types').PageLoad} */
-// export const load = async ({ fetch }) => {
-//   const fetchEvents = async () => {
-//     const dbUrl =
-//       'https://activefire-2e9a0-default-rtdb.europe-west1.firebasedatabase.app/fire_weather.json';
-//     const eventsResult = await fetch(dbUrl);
-//     const eventsData = await eventsResult.json();
-//     return eventsData;
-//   };
-//
-//   return {
-//     events: fetchEvents()
-//   };
-// };
+/** @type {import('./$types').PageLoad} */
+export const load = async ({ fetch }) => {
+  const fetchEvents = async () => {
+    const dbUrl =
+      'https://activefire-2e9a0-default-rtdb.europe-west1.firebasedatabase.app/fire_weather.json';
+    const eventsResult = await fetch(dbUrl);
+    const eventsData = await eventsResult.json();
+    return eventsData;
+  };
+
+  return {
+    events: fetchEvents()
+  };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,45 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
-	import Spiral from '$lib/components/time-spiral/Spiral.svelte';
-	import Selector from '$lib/components/Selector.svelte';
-	import data from '$lib/data/fire_weather_region_int_2023_11_02.json';
-	import { variables, selectedVariable, regions, selectedRegion } from '$lib/stores/selectStore.js';
-	// /** @type {import('./$types').PageData} */
-	// export let data;
+  import Spiral from "$lib/components/time-spiral/Spiral.svelte";
+  // import data from '$lib/data/fire_weather_region_int_2023_11_02.json';
+  import {
+    variables,
+    selectedVariable,
+    regions,
+    selectedRegion,
+  } from "$lib/stores/selectStore.js";
+  /** @type {import('./$types').PageData} */
+  export let data;
 
-	let w, h;
-	let isLoading = false;
-	$: filteredData = data[$selectedRegion];
+  let w, h;
+  let isLoading = false;
+  $: filteredData = data.events[$selectedRegion];
 </script>
 
 <div class="h-full flex relative">
-	{#if isLoading}
-		<div class="loading">Loading...</div>
-	{:else if filteredData.length}
-		<div class="h-full w-full" bind:clientWidth={w} bind:clientHeight={h}>
-			<Spiral data={filteredData} z={$selectedVariable[0]} parentWidth={w} parentHeight={h} />
-		</div>
-	{:else}
-		<p>No data</p>
-	{/if}
+  {#if isLoading}
+    <div class="loading">Loading...</div>
+  {:else if filteredData.length}
+    <div class="h-full w-full" bind:clientWidth={w} bind:clientHeight={h}>
+      <Spiral
+        data={filteredData}
+        z={$selectedVariable[0]}
+        parentWidth={w}
+        parentHeight={h}
+      />
+    </div>
+  {:else}
+    <p>No data</p>
+  {/if}
 </div>
 
 <style>
-	.loading {
-		flex: 1;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		font-size: 0.8em;
-		color: #999;
-		font-style: italic;
-	}
+  .loading {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8em;
+    color: #999;
+    font-style: italic;
+  }
 </style>
