25d0bfc903058577a1adf39d94299ce379d753cb
tadasnik <tadas.nik@gmail.com>
2024-03-05 14:23:16 +0000

    supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ffxiscwkuramynmzymav.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeGlzY3drdXJhbXlubXp5bWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyODE4MzIsImV4cCI6MjAyMTg1NzgzMn0.1ORv-s9lDA54FyhrlgW8S6iluES0uhybYx8BB5QuhSk')
+// export const supabase = createClient('https://ffxiscwkuramynmzymav.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeGlzY3drdXJhbXlubXp5bWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyODE4MzIsImV4cCI6MjAyMTg1NzgzMn0.1ORv-s9lDA54FyhrlgW8S6iluES0uhybYx8BB5QuhSk')
+export const supabase = createClient('https://ffxiscwkuramynmzymav.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeGlzY3drdXJhbXlubXp5bWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyODE4MzIsImV4cCI6MjAyMTg1NzgzMn0.1ORv-s9lDA54FyhrlgW8S6iluES0uhybYx8BB5QuhSk')
